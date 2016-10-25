e7d3eb44c2af2a15c1243d944ce8255f17c42302
Amritanshu Agrawal <aagrawa8@ncsu.edu>
2016-10-25 16:31:19 -0400

    code5

---
+++ code/5/CompareLearners.py
@@ -0,0 +1,128 @@
+from __future__ import print_function, division
+from Table import Table
+from random import random,shuffle,randint,seed
+import time
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.neighbors import KDTree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sk import rdivDemo
+from ABCD import ABCD
+
+
+sys.dont_write_bytecode = True
+
+def train_test(corpus, folds=5, index=0):
+    i_major = []
+    i_minor = []
+    l = len(corpus)
+    corpus=pd.DataFrame({"A":corpus})
+    for i in range(0, folds):
+        if i == index:
+            i_minor.extend(range(int(i * l / folds), int((i + 1) * l / folds)))
+        else:
+            i_major.extend(range(int(i * l / folds), int((i + 1) * l / folds)))
+    return list(corpus.iloc[i_major,].values.flatten()), list(corpus.iloc[i_minor,].values.flatten())
+
+if __name__ == '__main__':
+    datasets=['diabetes.csv','camel-1.0.csv']
+    folds=5
+    seed(1)
+    rec_dic={}
+    fa_dic={}
+    runtime={}
+    trees=['NB','mini','kd']
+    for k in datasets:
+        rec_dic[k]={}
+        fa_dic[k] = {}
+        runtime[k]={}
+        for x in trees:
+            rec_dic[k][x]=[]
+            fa_dic[k][x] = []
+            runtime[k][x] = 0
+        print("### Dataset: "+k+ " ###\n")
+        csvFile = './data/'+k
+        table = Table(csvFile)
+        for i in xrange(folds):
+            shuffle(table.rows)
+            for index in xrange(folds):
+                train,test=train_test(table.rows, folds=folds, index=index)
+                data_train=[]
+                data_test = []
+                train_label = []
+                test_label = []
+                for x in train:
+                    train_label.append(x[-1])
+                    data_train.append(x[:-1])
+                for x in test:
+                    test_label.append(x[-1])
+                    data_test.append(x[:-1])
+
+                ### Minibatch ###
+                start_time = time.time()
+                kmeans = MiniBatchKMeans(init='random',n_clusters=20, n_init=20,batch_size=20,compute_labels=True)
+                kmeans.fit(data_train,train_label)
+                k_means_cluster_centers = np.sort(kmeans.cluster_centers_, axis=0)
+                k_means_labels = pairwise_distances_argmin(data_train, k_means_cluster_centers)
+                predict=kmeans.predict(data_test)
+                prediction=[]
+                for y in predict:
+                    prediction.append(train_label[y])
+                abcd = ABCD(before=test_label, after=prediction)
+                # recall for 1st target class
+                #print(np.array([j.stats()[0] for j in abcd()]))
+                rec_dic[k]['mini'].append(np.array([j.stats()[0] for j in abcd()])[0])
+                # false alarm for 1st target class
+                fa_dic[k]['mini'].append(([j.stats()[1] for j in abcd()])[0])
+                runtime[k]['mini'] += time.time() - start_time
+
+
+                ### KD Tree ###
+                start_time = time.time()
+                tree = KDTree(data_train, metric='euclidean')
+                _, ind=tree.query(data_test,k=1)
+                #print(ind)
+                predict=[item for sublist in ind for item in sublist]
+
+                for y in predict:
+                    prediction.append(train_label[y])
+                abcd = ABCD(before=test_label, after=prediction)
+                # recall for 1st target class
+
+                rec_dic[k]['kd'].append(np.array([j.stats()[0] for j in abcd()])[0])
+                # false alarm for 1st target class
+                fa_dic[k]['kd'].append(([j.stats()[1] for j in abcd()])[0])
+                runtime[k]['kd'] += time.time() - start_time
+
+                ### NB ###
+                start_time = time.time()
+                NB = GaussianNB()
+                NB.fit(data_train,train_label)
+                predict=NB.predict(data_test)
+
+                abcd = ABCD(before=test_label, after=predict)
+                # recall for 1st target class
+                rec_dic[k]['NB'].append(np.array([j.stats()[0] for j in abcd()])[0])
+                # false alarm for 1st target class
+                fa_dic[k]['NB'].append(([j.stats()[1] for j in abcd()])[0])
+                runtime[k]['NB'] += time.time() - start_time
+    print(rec_dic)
+    print(fa_dic)
+    print(runtime)
+
+    ## scottknot code taken from Dr Menzies
+    for feature in datasets:
+        tmp = []
+        temp1=[]
+        print(feature)
+        for is_shingle in trees:
+                tmp.append([is_shingle] + rec_dic[feature][is_shingle])
+                temp1.append([is_shingle] + fa_dic[feature][is_shingle])
+        print("Recall")
+        print(rdivDemo(tmp))
+        print("False Alarm")
+        print(rdivDemo(temp1))
+        print("\n")
--- code/5/README.md
+++ code/5/README.md
@@ -1,1 +1,53 @@
 # Week 5
+- Datasets used are Camel-1.0.csv which was downloaded from [Promise Repository] (http://openscience.us/repo/defect/ck/camel.html) and other one is diabetes.csv provided with ninja
+- Datasets are available [here] (https://github.com/amritbhanu/fss16591/tree/master/code/4/data)
+- Experiments are ran with 5 fold cross val ie. 80% training and 20% testing
+- Results reported are false alarm, recall and runtimes.
+- Scott-knot analysis is done to generate pretty figures.
+- Run the code with **python CompareLearners.py**
+- All predictions on test data are done with k=1 for the 2 trees ie, Minibatch K means, KD tree and compared against Naive Bayes.
+## Results
+
+### Scott-knott analysis
+```
+diabetes.csv
+
+Recall
+rank ,         name ,    med   ,  iqr 
+----------------------------------------------------
+   1 ,         mini ,    0.66  ,  0.10 (          ---* |              ), 0.62,  0.66,  0.72
+   1 ,           kd ,    0.66  ,  0.10 (          ---* |              ), 0.62,  0.66,  0.72
+   2 ,           NB ,    0.84  ,  0.03 (               |       -*     ), 0.83,  0.84,  0.86
+
+False Alarm
+rank ,         name ,    med   ,  iqr 
+----------------------------------------------------
+   1 ,           NB ,    0.40  ,  0.07 (    --*        |              ), 0.37,  0.40,  0.44
+   2 ,         mini ,    0.65  ,  0.17 (               | -----*       ), 0.58,  0.65,  0.75
+   2 ,           kd ,    0.65  ,  0.17 (               | -----*       ), 0.58,  0.65,  0.75
+
+
+camel-1.0.csv
+
+Recall
+rank ,         name ,    med   ,  iqr 
+----------------------------------------------------
+   1 ,           NB ,    0.94  ,  0.06 (               |           -* ), 0.91,  0.94,  0.97
+   1 ,         mini ,    1.00  ,  0.03 (               |             *), 0.97,  1.00,  1.00
+   1 ,           kd ,    1.00  ,  0.03 (               |             *), 0.97,  1.00,  1.00
+   
+False Alarm
+rank ,         name ,    med   ,  iqr 
+----------------------------------------------------
+   1 ,           NB ,    0.50  ,  0.67 (--------------*|              ), 0.00,  0.50,  0.67
+   2 ,         mini ,    1.00  ,  0.00 (               |             *), 1.00,  1.00,  1.00
+   2 ,           kd ,    1.00  ,  0.00 (               |             *), 1.00,  1.00,  1.00
+
+```
+
+### Runtimes of just 1 cross val in seconds.
+- 'diabetes.csv': {'mini': 1.1872940063476562, 'NB': 0.05073714256286621, 'kd': 0.05971837043762207}
+- 'camel-1.0.csv': {'mini': 0.8251402378082275, 'NB': 0.04028034210205078, 'kd': 0.040816307067871094}
+
+### Answers
+- NB is faster than the fastest knn but NBs performance affects.
