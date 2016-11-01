7636af4b652c842b3c0896d11d83acac4f834e6a
rpotluri12 <raghav.potluri12@gmail.com>
2016-10-31 22:06:34 -0400

    bug fixed in selection of test and train data

--- code/6/DataGenerator.py
+++ code/6/DataGenerator.py
@@ -19,7 +19,6 @@ class DataGenerator(object):
         self.randomDataType2 = []
         self.random_type1()
         self.random_type2()
-        print len(self.randomDataType1), len(self.randomDataType2)
         self.newData = self.randomDataType1 + self.randomDataType2
 
     def random_type1(self):
@@ -71,38 +70,43 @@ class DataGenerator(object):
 
     def naiveBayes(self):
         for i in xrange(1, 21):
-            print len(self.newData)
-            print "Era #" + str(i) + ":"
+            # print len(self.newData)
             startIndex = (i-1)*100
             endIndex = (i)*100
-            train = self.newData[startIndex:((endIndex/2)-1)]
-            test = self.newData[(endIndex/2):endIndex]
+            train = self.newData[startIndex:((startIndex + endIndex/2)-1)]
+            test = self.newData[(startIndex + endIndex)/2:endIndex]
+            # print train, test
             train_label = []
             test_label = []
             data_train = []
             data_test = []
+            labelMap = {
+                'Iris-setosa': -1,
+                'Iris-versicolor': 0,
+                'Iris-virginica': 1
+            }
             for x in train:
-                train_label.append(x[-1])
+                train_label.append(labelMap[x[-1]])
                 data_train.append(x[:-1])
             for x in test:
-                test_label.append(x[-1])
+                test_label.append(labelMap[x[-1]])
                 data_test.append(x[:-1])
             NB = GaussianNB()
-            NB.fit(data_train,train_label)
+            NB.fit(np.array(data_train),train_label)
             predict=NB.predict(data_test)
-            print predict
-            # abcd = ABCD(before=test_label, after=predict)
+            # print predict
+            abcd = ABCD(before=test_label, after=predict)
             # recall for 1st target class
-            # print np.array([j.stats()[0] for j in abcd()])[0]
+            print "Era #" + str(i) + ":" + str(np.array([j.stats()[0] for j in abcd()])[0])
 
     # Dr.Menzies code.
     def a12(list1, list2):
         more = same = 0.0
         for x in sorted(list1):
             for y in sorted(list2):
-                if   x==y :
+                if x == y:
                     same += 1
-                elif x > y :
+                elif x > y:
                     more += 1
         return (more + 0.5*same) / (len(list1)*len(list2))
 
